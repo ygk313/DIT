09d8a2b8f9126e209d218baa8215ad5e84ae79f5
ygk313 <ygk313@naver.com>
2023-07-13 21:09:02 +0900

    fix: add <form> tag in detail.html

--- DIT/settings.py
+++ DIT/settings.py
@@ -132,10 +132,7 @@ LOGOUT_REDIRECT_URL = 'main'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'DIT', 'static')
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
